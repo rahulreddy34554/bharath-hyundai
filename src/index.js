36d0a47e783f08249d04b1d0068c409b222d52f1
rahulreddy34554 <rahulreddyallala6@gmail.com>
2025-03-25 10:36:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
+import ThankYouPage from "./ThankYouPage"; // Import ThankYouPage
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Router>
+    <Routes>
+      <Route path="/thank-you" element={<ThankYouPage/>} />
+      <Route path="/*" element={<App />} /> {/* Catch-all route for other pages */}
+    </Routes>
+  </Router>
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
